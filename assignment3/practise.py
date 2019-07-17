19c86bf1428693588094343c7a2b395887101e80
SakdaMrp <52265511+SakdaMrp@users.noreply.github.com>
2019-07-17 12:01:56 +0700

    Create practise.py

---
+++ assignment3/practise.py
@@ -0,0 +1,12 @@
+#added together should be 100
+my_list = [90,5,2.5,2.5]
+print("Result of my_list is: ", sum(my_list));
+
+#single value
+my_tuble = (666)
+print("Result of my_tuble is: ", my_tuble);
+
+#set1 intersection set2 return {5, 77, 9, 12}
+set1 = {5,10,20,35,77,9,100,50,12,19}
+set2 = {1,2,3,5,15,77,25,9,30,12}
+print("Result of intersection is: ", set1.intersection(set2));
