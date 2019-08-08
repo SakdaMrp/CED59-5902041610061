f616fc3a15c66d13e4ae863ada02f86f1d7f3933
Computer Education <computereducation@Macmini-213-13.local>
2019-08-08 16:34:52 +0700

    update assignment7

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,6 +1,12 @@
 from flask import Flask,render_template,request,redirect,url_for
-#import pymysql
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+
+#import pymysql
 app = Flask(__name__)
 #conn = pymysql.connect('localhost','root','','register_db')
 
@@ -15,6 +21,13 @@ def member():
 @app.route('/member',methods=["POST"])
 def register():
     return 'xyz';
+
+@app.route('/member2', methods=('GET', 'POST'))
+def submit():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect('/member')
+    return render_template('registerFrm.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
